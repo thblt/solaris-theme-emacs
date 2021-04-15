d706bc159bbfbc68f87c2fe830e197a6370b4441
Thibault Polge <thibault@thb.lt>
2021-04-15 14:43:20 +0200

    Better headings for code buffers

--- solaris.el
+++ solaris.el
@@ -27,34 +27,38 @@
 
 ;;; Code
 
+(defun solaris--heading-height (x) (if solarized-scale-outline-headlines x 1.0))
+
 (defvar solaris-theme
   '("Solaris, like Solarized, but with less fruit salad."
     ;; `default :foreground ,base0 :background ,base03
-    (let
-        ((ol-gray-blocks-1 `((,class (:inherit variable-pitch :background ,base02 :height ,(solaris--heading-height 1.6) :foreground ,base0 :weight normal :slant normal :overline nil :underline ,base01 :extend t))))
-         (ol-gray-blocks-2 `((,class (:inherit variable-pitch :background ,base02 :height ,(solaris--heading-height 1.4) :foreground ,magenta-1fg :weight normal :slant normal :overline nil :underline ,base01 :extend t))))
-         (ol-gray-blocks-3 `((,class (:inherit variable-pitch :background ,base02 :height ,(solaris--heading-height 1.2) :foreground ,violet-1fg :weight normal :slant normal :overline nil :underline ,base01 :extend t))))
-         (ol-gray-blocks-4 `((,class (:inherit variable-pitch :background ,base02 :height ,(solaris--heading-height 1.0) :foreground ,blue-1fg :weight normal :slant italic :overline nil :underline ,base01 :extend t))))
-         (ol-gray-blocks-5 `((,class (:inherit variable-pitch :background ,base02 :height ,(solaris--heading-height 1.0) :foreground ,cyan-1fg :weight bold   :slant normal :overline nil :underline ,base01 :extend t))))
-         (ol-gray-blocks-6 `((,class (:inherit variable-pitch :background ,base02 :height ,(solaris--heading-height 1.0) :foreground ,green-1fg :weight normal :slant normal :overline nil :underline ,base01 :extend t))))
-         (ol-gray-blocks-7 `((,class (:inherit variable-pitch :background ,base02 :height ,(solaris--heading-height 1.0) :foreground ,yellow-1fg :weight normal :slant italic :overline nil :underline ,base01 :extend t))))
-         (ol-gray-blocks-8 `((,class (:inherit variable-pitch :background ,base02 :height ,(solaris--heading-height 1.0) :foreground ,orange-1fg :weight normal :slant italic :overline nil :underline nil) :extend t)))
+    (let*
+        ((dark (eq variant 'dark))
+         ;; Headings for code
+         (code-heading-1 `((,class ( :foreground ,red-1fg     :inherit variable-pitch :background ,base02 :weight bold :overline nil :underline ,base01 :extend t))))
+         (code-heading-2 `((,class ( :foreground ,magenta-1fg :inherit variable-pitch :background ,base02 :weight bold :overline nil :underline ,base01 :extend t))))
+         (code-heading-3 `((,class ( :foreground ,violet-1fg  :inherit variable-pitch :background ,base02 :weight bold :overline nil :underline ,base01 :extend t))))
+         (code-heading-4 `((,class ( :foreground ,blue-1fg    :inherit variable-pitch :background ,base02 :weight bold :overline nil :underline ,base01 :extend t))))
+         (code-heading-5 `((,class ( :foreground ,cyan-1fg    :inherit variable-pitch :background ,base02 :weight bold :overline nil :underline ,base01 :extend t))))
+         (code-heading-6 `((,class ( :foreground ,green-1fg   :inherit variable-pitch :background ,base02 :weight bold :overline nil :underline ,base01 :extend t))))
+         (code-heading-7 `((,class ( :foreground ,yellow-1fg  :inherit variable-pitch :background ,base02 :weight bold :overline nil :underline ,base01 :extend t))))
+         (code-heading-8 `((,class ( :foreground ,orange-1fg  :inherit variable-pitch :background ,base02 :weight bold :overline nil :underline nil)    :extend t)))
          ;; ol-colors
-         (ol-color-blocks-1 `((,class (:inherit variable-pitch :foreground ,base02 :distant-foreground ,base2 :height ,(solaris--heading-height 1.6) :background ,base0       :weight normal :slant normal :extend t))))
-         (ol-color-blocks-2 `((,class (:inherit variable-pitch :foreground ,base02 :distant-foreground ,base2 :height ,(solaris--heading-height 1.4) :background ,magenta-1bg :weight normal :slant normal :extend t))))
-         (ol-color-blocks-3 `((,class (:inherit variable-pitch :foreground ,base02 :distant-foreground ,base2 :height ,(solaris--heading-height 1.2) :background ,violet-1bg  :weight normal :slant normal :extend t))))
-         (ol-color-blocks-4 `((,class (:inherit variable-pitch :foreground ,base02 :distant-foreground ,base2 :height ,(solaris--heading-height 1.0) :background ,blue-1bg    :weight normal :slant normal :extend t))))
-         (ol-color-blocks-5 `((,class (:inherit variable-pitch :foreground ,base02 :distant-foreground ,base2 :height ,(solaris--heading-height 1.0) :background ,cyan-1bg    :weight bold   :slant normal :extend t))))
-         (ol-color-blocks-6 `((,class (:inherit variable-pitch :foreground ,base02 :distant-foreground ,base2 :height ,(solaris--heading-height 1.0) :background ,green-1bg   :weight normal :slant normal :extend t))))
-         (ol-color-blocks-7 `((,class (:inherit variable-pitch :foreground ,base02 :distant-foreground ,base2 :height ,(solaris--heading-height 1.0) :background ,yellow-1bg  :weight normal :slant italic :extend t))))
-         (ol-color-blocks-8 `((,class (:inherit variable-pitch :foreground ,base02 :distant-foreground ,base2 :height ,(solaris--heading-height 1.0) :background ,orange-1bg  :weight normal :slant italic :extend t)))))
+         (doc-heading-1 `((,class (:inherit variable-pitch :foreground ,base02 :distant-foreground ,base2 :height ,(solaris--heading-height 1.6) :background ,base0       :weight normal :slant normal :extend t))))
+         (doc-heading-2 `((,class (:inherit variable-pitch :foreground ,base02 :distant-foreground ,base2 :height ,(solaris--heading-height 1.4) :background ,magenta-1bg :weight normal :slant normal :extend t))))
+         (doc-heading-3 `((,class (:inherit variable-pitch :foreground ,base02 :distant-foreground ,base2 :height ,(solaris--heading-height 1.2) :background ,violet-1bg  :weight normal :slant normal :extend t))))
+         (doc-heading-4 `((,class (:inherit variable-pitch :foreground ,base02 :distant-foreground ,base2 :height ,(solaris--heading-height 1.0) :background ,blue-1bg    :weight normal :slant normal :extend t))))
+         (doc-heading-5 `((,class (:inherit variable-pitch :foreground ,base02 :distant-foreground ,base2 :height ,(solaris--heading-height 1.0) :background ,cyan-1bg    :weight bold   :slant normal :extend t))))
+         (doc-heading-6 `((,class (:inherit variable-pitch :foreground ,base02 :distant-foreground ,base2 :height ,(solaris--heading-height 1.0) :background ,green-1bg   :weight normal :slant normal :extend t))))
+         (doc-heading-7 `((,class (:inherit variable-pitch :foreground ,base02 :distant-foreground ,base2 :height ,(solaris--heading-height 1.0) :background ,yellow-1bg  :weight normal :slant italic :extend t))))
+         (doc-heading-8 `((,class (:inherit variable-pitch :foreground ,base02 :distant-foreground ,base2 :height ,(solaris--heading-height 1.0) :background ,orange-1bg  :weight normal :slant italic :extend t)))))
       (custom-theme-set-faces
        theme-name
-       ;;;; display-line-numbers
+       ;; display-line-numbers
        `(line-number-minor-tick ((,class (:inherit line-number :weight normal))))
        `(line-number-major-tick ((,class (:inherit line-number-minor-tick :weight bold))))
        `(line-number-current-line ((,class (:inherit line-number :background ,base03 :foreground ,base0))))
-       ;;;; font lock
+       ;; font lock
        `(font-lock-comment-face ((,class (:foreground ,green :slant italic))))
        `(font-lock-comment-delimiter-face ((,class (:foreground ,green :slant italic))))
        `(font-lock-constant-face ((,class (:slant italic))))
@@ -65,12 +69,12 @@
        `(font-lock-string-face ((,class (:slant italic))))
        `(font-lock-type-face ((,class (:underline t))))
        `(font-lock-variable-name-face ((,class ())))
-       ;;;; modeline
-       ;; `(mode-line ((,class (:foreground ,base01 :background ,base3 :underline ,base02 :overline ,base02))))
-       ;; `(mode-line-highlight ((,class (:foreground ,base01 :weight ,s-maybe-bold))))
-       ;; `(mode-line-inactive ((,class (:backround ,base02 :foreground ,base01 :underline ,base01 :overline ,base01))))
-       ;;       `(mode-line-buffer-id ((,class (:inverse-video t :weight bold))))
-       `(header-line ((,class (:background ,base02 :underline ,base01))))
+       ;; modeline (and header)
+       `(mode-line          ((,class (:foreground ,base2 :background ,base02 :underline nil :overline nil :box nil))))
+       `(mode-line-inactive ((,class (:foreground ,base01 :background ,base03 :underline ,base02 :overline ,base02 :box nil))))
+       `(mode-line-highlight ((,class (:foreground ,base01 :weight ,s-maybe-bold :inverse-video t))))
+       ;; `(mode-line-buffer-id ((,class (:inherit mode-line :weight bold))))
+       `(header-line ((,class (:inherit mode-line :overline nil))))
        ;;;; erc
        `(erc-notice-face ((,class (:foreground ,base01))))
        `(erc-prompt-face ((,class (:background ,base02 :foreground ,base0))))
@@ -88,22 +92,14 @@
        ;; mu4e
        `(mu4e-unread-face ((,class (:weight bold :foreground ,base1))))
        ;;;; org
-       ;; `(org-level-1 ,ol-gray-blocks-1)
-       ;; `(org-level-2 ,ol-gray-blocks-2)
-       ;; `(org-level-3 ,ol-gray-blocks-3)
-       ;; `(org-level-4 ,ol-gray-blocks-4)
-       ;; `(org-level-5 ,ol-gray-blocks-5)
-       ;; `(org-level-6 ,ol-gray-blocks-6)
-       ;; `(org-level-7 ,ol-gray-blocks-7)
-       ;; `(org-level-8 ,ol-gray-blocks-8)
-       `(org-level-1 ,ol-color-blocks-1)
-       `(org-level-2 ,ol-color-blocks-2)
-       `(org-level-3 ,ol-color-blocks-3)
-       `(org-level-4 ,ol-color-blocks-4)
-       `(org-level-5 ,ol-color-blocks-5)
-       `(org-level-6 ,ol-color-blocks-6)
-       `(org-level-7 ,ol-color-blocks-7)
-       `(org-level-8 ,ol-color-blocks-8)
+       `(org-level-1 ,doc-heading-1)
+       `(org-level-2 ,doc-heading-2)
+       `(org-level-3 ,doc-heading-3)
+       `(org-level-4 ,doc-heading-4)
+       `(org-level-5 ,doc-heading-5)
+       `(org-level-6 ,doc-heading-6)
+       `(org-level-7 ,doc-heading-7)
+       `(org-level-8 ,doc-heading-8)
        `(org-todo ((,class (:foreground ,base02 :background ,red-1bg :foreground ,red-1fg :box ,red-1fg :underline ,base01))))
        `(org-done ((,class (:foreground ,base02 :background ,green-1bg :foreground ,green-1fg :box ,green-1fg :underline ,base01))))
        ;; @FIXME style org-headline-todo org-headline-done
@@ -112,22 +108,22 @@
        `(org-block-end-line ((,class (:inherit org-block-begin-line :underline nil :overline ,base01))))
        `(org-table ((,class (:background ,base02 foreground ,base0))))
        ;;;; outshine/outline-minor
-       `(outline-minor-1 ,ol-color-blocks-1)
-       `(outline-minor-2 ,ol-color-blocks-2)
-       `(outline-minor-3 ,ol-color-blocks-3)
-       `(outline-minor-4 ,ol-color-blocks-4)
-       `(outline-minor-5 ,ol-color-blocks-5)
-       `(outline-minor-6 ,ol-color-blocks-6)
-       `(outline-minor-7 ,ol-color-blocks-7)
-       `(outline-minor-8 ,ol-color-blocks-8)
-       `(outshine-level-1 ,ol-gray-blocks-1)
-       `(outshine-level-2 ,ol-gray-blocks-2)
-       `(outshine-level-3 ,ol-gray-blocks-3)
-       `(outshine-level-4 ,ol-gray-blocks-4)
-       `(outshine-level-5 ,ol-gray-blocks-5)
-       `(outshine-level-6 ,ol-gray-blocks-6)
-       `(outshine-level-7 ,ol-gray-blocks-7)
-       `(outshine-level-8 ,ol-gray-blocks-8)
+       `(outline-minor-1 ,code-heading-1)
+       `(outline-minor-2 ,code-heading-2)
+       `(outline-minor-3 ,code-heading-3)
+       `(outline-minor-4 ,code-heading-4)
+       `(outline-minor-5 ,code-heading-5)
+       `(outline-minor-6 ,code-heading-6)
+       `(outline-minor-7 ,code-heading-7)
+       `(outline-minor-8 ,code-heading-8)
+       `(outshine-level-1 ,code-heading-1)
+       `(outshine-level-2 ,code-heading-2)
+       `(outshine-level-3 ,code-heading-3)
+       `(outshine-level-4 ,code-heading-4)
+       `(outshine-level-5 ,code-heading-5)
+       `(outshine-level-6 ,code-heading-6)
+       `(outshine-level-7 ,code-heading-7)
+       `(outshine-level-8 ,code-heading-8)
        ;;;; smartparens
        `(sp-show-pair-mismatch-face ((,class (:background ,red :foreground ,yellow :extend t))))
        ;;;; which-function
